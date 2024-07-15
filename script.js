4d5bba5feac05cdaa7c8e84704d23c190c660ab7
Nevin <nevinsajie@gmail.com>
2024-07-15 19:51:29 +0530

    added js for changing size

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ let stream;
 const video=document.getElementById('my-video');
 const btn=document.getElementById('share').addEventListener('click',async(e)=>{
     stream=await navigator.mediaDevices.getUserMedia(access);
-    console.log(stream);
-    console.log(stream.getTracks());
+    const tracks=stream.getTracks();
+    tracks.forEach(element => {
+        console.log(element);
+        
+    });
 
 });
 
@@ -21,4 +24,18 @@ const btn3=document.getElementById('stop-video').addEventListener('click',(e)=>{
         
     });
 })
+function changesize() {
+    const tracks=stream.getTracks();
+    const height=document.getElementById('vid-height').value;
+    const width=document.getElementById('vid-width').value;
+    console.log(height);
+    tracks.forEach(element => {
+        const constraints={
+            height:height,
+            width:width,
+        }
+        element.applyConstraints(constraints);
+        
+    });
+};
 
